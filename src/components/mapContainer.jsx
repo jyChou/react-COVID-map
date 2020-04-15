652487a5dd8da536094b8995a3859a094df534da
jyChou <a23064569@gmail.com>
2020-04-14 23:26:36 -0400

    fix recenter issue

--- src/components/mapContainer.jsx
+++ src/components/mapContainer.jsx
@@ -7,27 +7,38 @@ class MapContainer extends Component {
   state = {
     marker_id: false,
     loading: true,
+    defaultLat: 40.756795,
+    defaultLng: -74.005974,
   };
   handleOnClick = (id) => {
-    this.setState({ marker_id: id });
+    const c = this.props.cases.filter((c) => c.id === id);
+    this.setState({
+      marker_id: c[0].id,
+      defaultLat: c[0].lat,
+      defaultLng: c[0].lng,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({ marker_id: 0 });
   };
   componentDidMount = () => {
-      console.log("close loading page!")
-      this.setState({ loading: false})
-  }
+    console.log("close loading page!");
+    this.setState({ loading: false });
+  };
   render() {
     return (
       <>
         <div style={{ height: "100%", width: "100%", position: "absolute" }}>
-          {this.state.loading ? (<Loading />) : ("")}
+          {this.state.loading ? <Loading /> : ""}
           <Map
             googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
             loadingElement={<div style={{ height: "100%" }} />}
             containerElement={<div style={{ height: "100%" }} />}
             mapElement={<div style={{ height: "100%" }} />}
             cases={this.props.cases}
-            defaultLat={this.props.defaultLat}
-            defaultLng={this.props.defaultLng}
+            defaultLat={this.state.defaultLat}
+            defaultLng={this.state.defaultLng}
             onClick={this.handleOnClick}
           />
         </div>
@@ -36,7 +47,7 @@ class MapContainer extends Component {
             data={this.props.cases}
             id={this.state.marker_id}
             onClick={() => {
-              this.handleOnClick(0);
+              this.handleClose();
             }}
           />
         ) : (
